df3583a31136be5a976fe2ca25ff28d48eddefe7
Pedro Lucas <lucasdasilvajunior@yahoo.com.br>
2016-03-21 23:07:43 -0300

    Compile to java 1.7

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,33 @@ const path = require('path');
 const sequence = require('run-sequence');
 const clean = require('gulp-clean');
 
-
 gulp.task('clear-test', function() {
 	return gulp.src('spec/tmp/page.pdf', {read: false}).pipe(clean());
 });
 
 gulp.task('test', ['clear-test'], function() {
-	gulp.src('spec/simple-test.js').pipe(jasmine({verbose:true}));
+	return gulp.src('spec/simple-test.js').pipe(jasmine({verbose:true}));
 });
 
-/*
 gulp.task('java-compile', shell.task([
   'javac -classpath .:"' + path.join(__dirname, 'src-library/*')
 	+ '" ' + path.join(__dirname, 'src-java/br/com/appmania/*.java')
 ]));
 
-gulp.task('compile', ['java-compile'], function() {
-	gutil.log(gutil.colors.green('Java classes compiled'));
+gulp.task('java-copy', function() {
+	return gulp.src('src-java/**/*.class')
+	.pipe(gulp.dest('out/production/node-pdfbox'));
 });
-*/
-gulp.task('default', ['test']);
+
+gulp.task('java-clean', function() {
+	return gulp.src('src-java/**/*.class')
+	.pipe(clean());
+});
+
+gulp.task('compile', function(done) {
+	sequence('java-compile', 'java-copy', 'java-clean', done);
+});
+
+gulp.task('default', function(done) {
+	sequence('compile', 'test', done);
+});
