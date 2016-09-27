15facfa639c60f332028382811e3274016a0b68d
Pedro Lucas <pedro@objota.com.br>
2016-09-27 13:09:03 -0300

    Added use strict in files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = require('./lib/pdf-document');
 module.exports.PDFPage = require('./lib/pdf-page');
 module.exports.PDFPageImage = require('./lib/pdf-page-image');
--- post-install.js
+++ post-install.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const exec = require('child_process').exec;
 const path = require('path');
 const javahome = require('find-java-home');
