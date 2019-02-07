ea25835e3a135d83fc83d819a08e0439cc718036
Ravi Chaurasia <ravi.chaurasia@goodworklabs.com>
2019-02-07 15:58:43 +0530

    fetch hyperlink util method added

--- src/main/java/br/com/appmania/Main.java
+++ src/main/java/br/com/appmania/Main.java
@@ -1,5 +1,10 @@
 package br.com.appmania;
 
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
 /**
  * Created by PedroLucas on 3/9/16.
  */
@@ -7,10 +12,11 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            PDFDocument doc = PDFDocument.load("spec/files/multi-page.pdf");
-            PDFPage page = doc.getPage(0);
-            PDFPageImage image = page.getImage(1, "png");
-            image.save("spec/tmp/image.png");
+            PDDocument doc = PDDocument.load(new File("spec/files/linktest.PDF"));
+            for(int i=0; i<doc.getNumberOfPages();i++) {
+            PDPage page = doc.getPage(i);
+            PDFPage.getLink(page);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return;
--- src/main/java/br/com/appmania/PDFPage.java
+++ src/main/java/br/com/appmania/PDFPage.java
@@ -1,15 +1,22 @@
 package br.com.appmania;
 
 import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.interactive.action.PDAction;
+import org.apache.pdfbox.pdmodel.interactive.action.PDActionURI;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 /**
  * Created by PedroLucas on 3/9/16.
@@ -98,6 +105,59 @@ public class PDFPage {
         return textStripper.getText(document);
     }
 
+    public static void getLink(PDPage page) throws IOException {    	
+    	    PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+    	    List<PDAnnotation> annotations = page.getAnnotations();
+    	    //System.out.println("Annotations:"+ annotations.size());
+    	    //first setup text extraction regions
+    	    for( int j=0; j<annotations.size(); j++ )
+    	    {
+    	        PDAnnotation annot = annotations.get(j);
+    	        if( annot instanceof PDAnnotationLink )
+    	        {
+    	            PDAnnotationLink link = (PDAnnotationLink)annot;
+    	            PDRectangle rect = link.getRectangle();
+    	            //need to reposition link rectangle to match text space
+    	            float x = rect.getLowerLeftX();
+    	            float y = rect.getUpperRightY();
+    	            float width = rect.getWidth();
+    	            float height = rect.getHeight();
+    	            int rotation = page.getRotation();
+    	            if( rotation == 0 )
+    	            {
+    	                PDRectangle pageSize = page.getMediaBox();
+    	                y = pageSize.getHeight() - y;
+    	            }
+    	            else if( rotation == 90 )
+    	            {
+    	                //do nothing
+    	            }
+
+    	            Rectangle2D.Float awtRect = new Rectangle2D.Float( x,y,width,height );
+    	            stripper.addRegion( "" + j, awtRect );
+    	        }
+    	    }
+
+    	    stripper.extractRegions( page );
+
+    	    for( int j=0; j<annotations.size(); j++ )
+    	    {
+    	        PDAnnotation annot = annotations.get(j);
+    	        if( annot instanceof PDAnnotationLink )
+    	        {
+    	            PDAnnotationLink link = (PDAnnotationLink)annot;
+    	            PDAction action = link.getAction();
+    	            String urlText = stripper.getTextForRegion( "" + j );
+    	            if( action instanceof PDActionURI )
+    	            {
+    	                PDActionURI uri = (PDActionURI)action;
+    	                System.out.println( "Page :" + urlText.trim() + "'=" + uri.getURI() );
+    	            }
+    	        }
+    	    }
+    	}
+
+    
     public PDPage getPage() {
         return page;
     }
